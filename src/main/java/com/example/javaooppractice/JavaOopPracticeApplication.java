7b427e586e91c474c23b84dc93789d910d4f6f57
Seongha <sseongha11@gmail.com>
2023-05-11 21:38:52 +0100

    Initialise the Project

---
+++ src/main/java/com/example/javaooppractice/JavaOopPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaooppractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOopPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaOopPracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaooppractice/JavaOopPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaooppractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaOopPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
