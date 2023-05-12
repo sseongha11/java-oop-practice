065d932ffc5c37d911d0436747fb76e2359812dc
Seongha <sseongha11@gmail.com>
2023-05-12 10:43:50 +0100

    Update #3 - Main
    #3 is completed

--- src/main/java/com/example/javaooppractice/Main.java
+++ src/main/java/com/example/javaooppractice/Main.java
@@ -1,12 +1,13 @@
 package com.example.javaooppractice;
 
 import com.example.javaooppractice.logic.BubbleSort;
+import com.example.javaooppractice.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new BubbleSort<>();
 
         System.out.println("[result " + sort.sort(Arrays.asList(args)));
     }
